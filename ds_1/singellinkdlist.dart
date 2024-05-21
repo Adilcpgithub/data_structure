0cd12fdd9f4bd84e6785649faf1dd2c0eed6d19e
Adilcpgithub <adilcp8590@email.com>
2024-05-21 21:17:38 +0530

    insert at position added

--- ds_1/singellinkdlist.dart
+++ ds_1/singellinkdlist.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 class Node<T> {
   T data;
   Node<T>? next;
@@ -8,12 +6,28 @@ class Node<T> {
 
 class SinglyLinkedList<T> {
   Node<T>? head;
+  Node<T>? tail;
   //----------------------------------------------------------------------------
   // Add a new node to the end of the list
+  void appendcorrect(T data) {
+    Node<T> newNode = Node(data);
+    if (head == null) {
+      head = newNode;
+      tail = newNode;
+    } else {
+      tail?.next = newNode; // Link the new node to the end of the list
+      tail = newNode; // Update the tail to the new node
+    }
+  }
+
+//----------------------
   void append(T data) {
     if (head == null) {
       head = Node(data);
+      tail = Node(data);
     } else {
+      tail?.next = Node(data); // Link the new node to the end of the list
+      tail = Node(data);
       Node<T>? current = head;
       while (current?.next != null) {
         current = current?.next;
@@ -50,14 +64,44 @@ class SinglyLinkedList<T> {
       head = head?.next;
       return;
     }
+
     Node<T>? current = head;
     while (current?.next != null) {
       if (current?.next?.data == data) {
         current?.next = current.next?.next;
+        if (tail?.data == data) {
+          tail = current;
+        }
         return;
       }
       current = current?.next;
     }
+  }
+
+  void insertAfter(T nextTo, T data) {
+    Node<T>? temp = head;
+    Node<T> putData = Node(data);
+
+    // Traverse to find the node with data 'nextTo'
+    while (temp != null && temp.data != nextTo) {
+      temp = temp.next;
+    }
+
+    // If the node with 'nextTo' data is not found, return
+    if (temp == null) {
+      return;
+    }
+
+    // If inserting after the tail node
+    if (temp == tail) {
+      tail?.next = putData;
+      tail = putData;
+      return;
+    }
+
+    // Insert the new node after the found node
+    putData.next = temp.next;
+    temp.next = putData;
   }
 }
 
@@ -68,7 +112,8 @@ void main() {
   sample.append(3);
   sample.append(4);
 
-  sample.delete(1);
+  sample.insertAfter(2, 22);
+  // sample.delete(1);
 
   sample.printdata();
 }
