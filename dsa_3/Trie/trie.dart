0c1e6d00b9d4f34282475e52506dc78a4dd20ec2
Adilcpgithub <adilcp8590@email.com>
2024-06-20 17:52:05 +0530

    basic trie added

--- dsa_3/Trie/trie.dart
+++ dsa_3/Trie/trie.dart
@@ -27,6 +27,50 @@ class Trie {
     }
     return current.endOfNode;
   }
+
+  // this fucton for prefix
+  bool searchprefix(String words) {
+    TrieNode current = root;
+    for (var char in words.split('')) {
+      if (!current.character.containsKey(char)) {
+        return false;
+      }
+      current = current.character[char]!;
+    }
+    return true;
+  }
+}
+
+//-------------------------------------------------------------------------------------------------------------
+class TrieSuffex {
+  TrieNode root = TrieNode();
+
+  insert(String word) {
+    for (var i = 0; i < word.length; i++) {
+      String char = word.substring(i);
+      _insertSuffex(char);
+    }
+  }
+
+  _insertSuffex(String suffix) {
+    TrieNode current = root;
+    for (var char in suffix.split('')) {
+      current.character.putIfAbsent(char, () => TrieNode());
+      current = current.character[char]!;
+    }
+    current.endOfNode = true;
+  }
+
+  bool? searchSuffix(String word) {
+    TrieNode current = root;
+    for (var char in word.split('')) {
+      if (!current.character.containsKey(char)) {
+        return false;
+      }
+      current = current.character[char]!;
+    }
+    return current.endOfNode;
+  }
 }
 
 void main() {
@@ -35,4 +79,9 @@ void main() {
   trie.insert('india');
   trie.insert('its');
   print(trie.search('adil'));
+  print(trie.searchprefix('ad'));
+  print('----------------\n');
+  TrieSuffex trieSuffex = TrieSuffex();
+  trieSuffex.insert('adil');
+  print(trieSuffex.searchSuffix('dil'));
 }
